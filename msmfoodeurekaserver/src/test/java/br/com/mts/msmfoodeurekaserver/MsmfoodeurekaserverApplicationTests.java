e349a91e2c72bbbe91223ada00f1504805291fc0
“mtstechnologies” <michael.trindade@hotmail.com>
2024-11-08 16:46:09 -0400

    eureka server created and configured

---
+++ msmfoodeurekaserver/src/test/java/br/com/mts/msmfoodeurekaserver/MsmfoodeurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.msmfoodeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmfoodeurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
