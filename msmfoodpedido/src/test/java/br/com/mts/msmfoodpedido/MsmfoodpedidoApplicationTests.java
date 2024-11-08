1156f72ae5ae5d3a3b1ccf5b4eae2a6a3813b60b
“mtstechnologies” <michael.trindade@hotmail.com>
2024-11-08 17:35:59 -0400

    created order microservices and their entities

---
+++ msmfoodpedido/src/test/java/br/com/mts/msmfoodpedido/MsmfoodpedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.msmfoodpedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmfoodpedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
