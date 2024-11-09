b2ef2230a147e06183541050a3c5372153c3fad6
“mtstechnologies” <michael.trindade@hotmail.com>
2024-11-08 20:18:02 -0400

    creating and configuring the gateway

---
+++ msmfoodgateway/src/main/java/br/com/mts/msmfoodgateway/MsmfoodgatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.mts.msmfoodgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmfoodgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmfoodgatewayApplication.class, args);
+	}
+
+}
---
+++ msmfoodgateway/src/test/java/br/com/mts/msmfoodgateway/MsmfoodgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.msmfoodgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmfoodgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
