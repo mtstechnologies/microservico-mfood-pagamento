5e892b7f7fdb0279af45308a3d24a2268693a2aa
“mtstechnologies” <michael.trindade@hotmail.com>
2024-11-21 15:07:00 -0400

    publishing message in rabbitmq

--- msmfoodpagamento/src/main/java/br/com/mts/msmfoodpagamento/amqp/PagamentoAMQPConfig.java
+++ msmfoodpagamento/src/main/java/br/com/mts/msmfoodpagamento/amqp/PagamentoAMQPConfig.java
@@ -24,8 +24,12 @@ public class PagamentoAMQPConfig {
 		return new RabbitAdmin(conexao);
 	}
 	
+	//criando a fila no rabbit e detectar que houve conexao
 	@Bean
 	public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin){
 		return event -> rabbitAdmin.initialize();
 	}
+	
+	//https://www.rabbitmq.com/docs/download
+	//docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:4.0-management
 }
