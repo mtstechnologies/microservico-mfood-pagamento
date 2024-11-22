e2438364852706be6e090fb0620749c9382a5158
“mtstechnologies” <michael.trindade@hotmail.com>
2024-11-22 18:44:39 -0400

    evaluation microservices created

---
+++ msmfoodavaliacao/src/test/java/br/com/mts/msmfoodavaliacao/MsmfoodavaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mts.msmfoodavaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmfoodavaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
